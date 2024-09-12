5eb3747fbbf96c10a968eb16a8c1f460f33614d8
SokWith <129967025+SokWith@users.noreply.github.com>
2024-09-12 08:41:25 +0800

    Create uploadtx.js

---
+++ functions/uploadtx.js
@@ -0,0 +1,51 @@
+// functions/uploadtx.js 微信开放平台
+export async function onRequest(context) {
+    const { request } = context;
+    const uploadUrl = "https://openai.weixin.qq.com/weixinh5/webapp/h774yvzC2xlB4bIgGfX2stc4kvC85J/cos/upload";
+    
+    const corsHeaders = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Max-Age': '86400', // 24 hours
+    };
+
+    if (request.method === 'OPTIONS') {
+        return new Response(null, {
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'POST, OPTIONS',
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Max-Age': '86400', // 24小时
+            },
+        });
+    }
+    let formData;
+    // 将请求体解析为 FormData 对象
+    formData = await request.formData();
+    let fileData;
+    // 创建新的 FormData 对象并添加文件
+    const newFormData = new FormData();
+    newFormData.append("media", fileData, fileData.name);
+    // 转发请求
+     const response = await fetch(uploadUrl, {
+            method: 'POST',
+        //    headers: request.headers,
+            body:    newFormData,
+        });
+
+        const jsonResponse = await response.json();
+        const resurl = jsonResponse.url;
+      // 返回响应
+      if(resurl){
+        return new Response(resurl, {
+            status: response.status,
+            headers: {
+                ...corsHeaders,
+                ...response.headers,
+            },
+        });
+    }
+      return new Response('https://github.com/SokWith', {
+        status: 500,
+        headers: corsHeaders,
+      });    
+}
