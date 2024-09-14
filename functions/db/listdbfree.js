880c9c6c130ff402a74ee13a59382e3ec0b708f3
SokWith <129967025+SokWith@users.noreply.github.com>
2024-09-14 08:25:53 +0800

    只返回图片URL

--- functions/db/listdbfree.js
+++ functions/db/listdbfree.js
@@ -6,7 +6,7 @@ export async function onRequest(context) {
   const m = url.searchParams.get('m');
   const n = url.searchParams.get('n');
 
-// 检查数据库连接是否已定义
+  // 检查数据库连接是否已定义
   if (!database) {
     return new Response('Database connection not found.', { status: 500 });
   }
@@ -19,27 +19,33 @@ export async function onRequest(context) {
     const photosStringArray = await ps.raw();
     
     let photosString  = photosStringArray.map(item => item[0]); // 假设字符串在数组的第一个位置
-  // 使用 join() 方法将数组元素连接成一个字符串，然后用 split() 方法以回车符分割
+    // 使用 join() 方法将数组元素连接成一个字符串，然后用 split() 方法以回车符分割
     const allurls = photosString.join('\n').split('\n'); 
     // 使用 filter() 方法移除所有空字符串
     const urls = allurls.filter(url => url.trim() !== '');
- 
-  // 如果获取的URL数量不足n个，则使用所获取的最大数量
-  const maxUrls = urls.length < n ? urls.length : n;
 
-  // 随机抽取n个URL
-  const selectedUrls = urls.sort(() => Math.random() - 0.5).slice(0, maxUrls);
+    // 定义图片后缀名
+    const imageExtensions = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.webp'];
 
-  return new Response(JSON.stringify(selectedUrls), {
-    headers: { 'Content-Type': 'application/json' },
-    status: 200
-  });
+    // 过滤掉非图片的 URL
+    const imageUrls = urls.filter(url => {
+      const extension = url.substring(url.lastIndexOf('.')).toLowerCase();
+      return imageExtensions.includes(extension);
+    });
 
-    
+    // 如果获取的URL数量不足n个，则使用所获取的最大数量
+    const maxUrls = imageUrls.length < n ? imageUrls.length : n;
+
+    // 随机抽取n个URL
+    const selectedUrls = imageUrls.sort(() => Math.random() - 0.5).slice(0, maxUrls);
+
+    return new Response(JSON.stringify(selectedUrls), {
+      headers: { 'Content-Type': 'application/json' },
+      status: 200
+    });
+
   } catch (error) {
     // 如果查询过程中出现错误，返回错误信息
     return new Response(error.message, { status: 500 });
   }
-
-
 }
